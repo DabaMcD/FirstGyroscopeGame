75c9b0b68fe7eac2be6f31268d6856f402e70025
gooberbobber <dabamcdoodle@gmail.com>
2019-09-02 14:29:59 -0400

    wip

--- app/src/main/java/com/example/ballbalancewithgyroscope/GameView.java
+++ app/src/main/java/com/example/ballbalancewithgyroscope/GameView.java
@@ -2,32 +2,36 @@ package com.example.ballbalancewithgyroscope;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
-    private Context context;
+    private Paint paint = new Paint();
+    private float x, y;
+
     public GameView(Context context) {
         super(context);
-        this.context = context;
     }
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        this.context = context;
     }
     public GameView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        this.context = context;
     }
     @Override
     protected void onDraw(Canvas canvas) {
-
+        canvas.drawCircle(200 + x, 200 + y, 30, paint);
 
         super.onDraw(canvas);
     }
-    public void draw() {
+    public void draw(float sx, float sy) {
+        x = sx;
+        y = sy;
         invalidate();
         requestLayout();
     }
--- app/src/main/java/com/example/ballbalancewithgyroscope/MainActivity.java
+++ app/src/main/java/com/example/ballbalancewithgyroscope/MainActivity.java
@@ -2,11 +2,23 @@ package com.example.ballbalancewithgyroscope;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private Thread gameThread;
     private GameView gameView;
+    private SensorManager sm;
+    private Sensor ax, ay, az;
+    private List<Sensor> al;
+    private float x, y, z, sensorX, sensorY, sensorZ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +26,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         gameView = findViewById(R.id.gameView);
+
+        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        al = sm.getSensorList(Sensor.TYPE_ACCELEROMETER);
+        System.out.println(al.size());
+
+        if (al.size() != 0) {
+            ax = al.get(0);
+            ay = al.get(1);
+            az = al.get(2);
+            sm.registerListener(this, ax, SensorManager.SENSOR_DELAY_GAME);
+            sm.registerListener(this, ay, SensorManager.SENSOR_DELAY_GAME);
+            sm.registerListener(this, az, SensorManager.SENSOR_DELAY_GAME);
+        }
+
+        x = y = sensorX = sensorY = 0;
 
         startGameThread();
     }
@@ -26,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                gameView.draw();
+                                gameView.draw(sensorX, sensorY);
                             }
                         });
                         Thread.sleep(Math.abs(previousMillis - System.currentTimeMillis() + 30));
@@ -37,5 +64,31 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         gameThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        sm.unregisterListener(this, ax);
+        sm.unregisterListener(this, ay);
+        sm.unregisterListener(this, az);
+
+        super.onPause();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        try {
+            Thread.sleep(16);
+            sensorX = sensorEvent.values[0];
+            sensorY = sensorEvent.values[1];
+            sensorZ = sensorEvent.values[2];
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
